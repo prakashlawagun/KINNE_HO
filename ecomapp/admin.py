317628ca1d752853c6e1f9dd64fa9865dcf570d7
prakashlawagun <prakashlawagun11@gmail.com>
2022-01-09 19:55:24 +0545

    Frist Commit

---
+++ ecomapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register([Customer,Cart,CartProduct,Order])
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+        prepopulated_fields = {"slug":("title",)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+        prepopulated_fields = {"slug":("title",)}
---
+++ ecomapp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import *
+
+app_name ="ecomapp"
+urlpatterns = [
+    path("",HomeView.as_view(),name='home'),
+    path("about/",AboutView.as_view(),name='about'),
+    path("contact/",ContactView.as_view(),name='contact'),
+    path("all-products/",AllProductView.as_view(),name='allproducts'),
+    path("productdetials/<slug:slug>",ProductDetail.as_view(),name='productdetials'),
+    path("add-to-cart/<int:pro_id>",AddToCartView.as_view(),name='addtocart'),
+    path("mycart/",MyCart.as_view(),name='mycart'),
+    path("managecart/<int:cp_id>",ManageCartView.as_view(),name="managecart"),
+    path("emptycart/",EmptyCartView.as_view(),name="emptycart"),
+    path("checkout/",CheckoutView.as_view(),name="checkout"),
+    path("register/",CustomerRegisterView.as_view(),name="register"),
+    path("logout/",CustomerLogoutView.as_view(),name="logout"),
+    path("login/",CustomerLoginView.as_view(),name="login"),
+    path("profile/",CustomerProfileView.as_view(),name="profile"),
+    path("profile/order-<int:pk>/", CustomerOrderDetailView.as_view(), name="customerorderdetail"),
+
+]
